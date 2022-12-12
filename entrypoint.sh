4c2c696381087d954e7adbedad1eab6b742da3f2
christopher schicho <christopher.schicho@gmail.com>
2022-12-12 14:22:40 +0100

    #1-create the ssh action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,28 +3,33 @@
 set -eu
 
 SSH_PATH="$HOME/.ssh"
-SSH_FILE_PATH="$SSH_PATH/ssh_key"
+SSH_KEY_PATH="$SSH_PATH/ssh_key"
+SSH_KNOWN_PATH="$SSH_PATH/known_hosts"
+COMMAND_PATH="$HOME/command.sh"
 
 if [ ! -d "$SSH_PATH" ]; then
   mkdir "$SSH_PATH"
 fi
 
-if [ ! -f "$SSH_FILE_PATH" ]; then
-  touch "$SSH_FILE_PATH"
+if [ ! -f "$SSH_KNOWN_PATH" ]; then
+  touch "$SSH_KNOWN_PATH"
 fi
 
-echo "$INPUT_KEY" > "$SSH_FILE_PATH"
+if [ ! -f "$SSH_KEY_PATH" ]; then
+  touch "$SSH_KEY_PATH"
+fi
+
+echo "$INPUT_KEY" >"$SSH_KEY_PATH"
 
 chmod 700 "$SSH_PATH"
-chmod 600 "$SSH_FILE_PATH"
+chmod 600 "$SSH_KEY_PATH"
+chmod 600 "$SSH_KNOWN_PATH"
 
-echo Connecting to ssh host
+echo "$INPUT_COMMAND" >"$COMMAND_PATH"
+echo "exit \$?" >>"$COMMAND_PATH"
 
-sh -c "ssh $INPUT_HOST -l $INPUT_USER -p $INPUT_PORT -i $SSH_FILE_PATH"
+echo Start exectuting remote command
 
-echo Connecting to ssh host success
-echo Executing command
+sh -c "ssh -i $SSH_KEY_PATH -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $COMMAND_PATH"
 
-sh -c "$INPUT_COMMAND"
-
-echo Executung command success
+echo Successfully executed remote command
