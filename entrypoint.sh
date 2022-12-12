a5e1dfae981963dd7f0b83819f34f9994173e65a
Christopher Schicho <57227765+c-schicho@users.noreply.github.com>
2022-12-12 13:15:18 +0100

    #1-create the ssh action (#6)

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,35 +7,43 @@ if [ ! -d "$SSH_PATH" ]; then
   mkdir "$SSH_PATH"
 fi
 
+if [ ! -f "$SSH_FILE_PATH" ]; then
+  touch "$SSH_FILE_PATH"
+fi
+
 echo "$INPUT_KEY" > "$SSH_FILE_PATH"
 
 chmod 700 "$SSH_PATH"
-chmod 600 "SSH_FILE_PATH"
+chmod 600 "$SSH_FILE_PATH"
 
 echo Connecting to ssh host
 
-timeout "$INPUT_SSH_TIMEOUT" "ssh $INPUT_HOST -l $INPUT_USER -p $INPUT_PORT -i $SSH_FILE_PATH"
+timeout "$INPUT_SSH_TIMEOUT" ssh $INPUT_HOST -l $INPUT_USER -p $INPUT_PORT -i $SSH_FILE_PATH
 
 if [ $? -eq 124 ]; then
   echo "SSH login timed out"
   exit 1
 fi
 
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
+  echo "SSH login successful"
+else
   echo "SSH login failed"
   exit 1
 fi
 
 echo Executing command
 
-timeout "$INPUT_COMMAND_TIMEOUT" "$INPUT_COMMAND"
+timeout "$INPUT_COMMAND_TIMEOUT" $INPUT_COMMAND
 
 if [ $? -eq 124 ]; then
   echo "SSH command execution timed out"
   exit 1
 fi
 
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
+  echo "SSH command execution successful"
+else
   echo "SSH command execution failed"
   exit 1
 fi
